e0d95356af585f220f715a82e7c3f56323db242c
P-minhyeok2 <jhje0420@naver.com>
2025-06-10 10:17:22 +0900

    [박민혁] 총 결제 금액에 따른 등급 업데이트 기능 구현

--- src/main/java/mapperInterface/CustMapper.java
+++ src/main/java/mapperInterface/CustMapper.java
@@ -10,15 +10,33 @@ import com.example.demo.model.CustDTO;
 @Mapper
 public interface CustMapper {
 
-
+	//로그인
 	@Select("select password from custom where cust_id = #{mdto.cust_id}")
 	String login(@Param("mdto")CustDTO cdto);
 	
+	// 로그인된 아이디 이름 찾기
 	@Select("select cust_nm from custom where cust_id = #{cust_id}")
 	String search_name(@Param("cust_id")String cust_id);
 	
+	// 총 구매 금액 업데이트
 	@Update("UPDATE custom SET tot_buy_amt = tot_buy_amt + #{amount}, upd_dtm = NOW() WHERE cust_id = #{cust_id}")
     void updateTotBuyAmt(@Param("cust_id") String cust_id, @Param("amount") int amount);
 	
+	// 등급 업데이트
+	@Update("UPDATE custom SET grade = #{grade} WHERE cust_id = #{cust_id}")
+	void updateGrade(@Param("cust_id") String cust_id, @Param("grade") String grade);
+	
+	// 누적 금액 기준 등급 조회
+	@Select("SELECT grade FROM grade_disc WHERE std_amt <= #{totBuyAmt} ORDER BY std_amt DESC LIMIT 1")
+	String findGradeTotBuyAmt(@Param("totBuyAmt") int totBuyAmt);
+	
+	// 누적 총 구매 금액 조회
+    @Select("SELECT tot_buy_amt FROM custom WHERE cust_id = #{cust_id}")
+	int getTotBuyAmt(String cust_id);
+	
+	
 	String selectGradeByCustId(String cust_id); //cust_id로 등급 찾기
+
+	
+	
 }
